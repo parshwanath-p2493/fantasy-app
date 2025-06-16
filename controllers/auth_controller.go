cc25d51d9e777481f5804e943adc73d57d5fceb4
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-06-16 22:11:44 +0530

    commit

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,1 +1,41 @@
 package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"fantasy-backend/database"
+	"fantasy-backend/models"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Signup(w http.ResponseWriter, r *http.Request) {
+	var userCollection = database.DB.Collection("users")
+	var user models.User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Check if user already exists
+	var existingUser models.User
+	err := userCollection.FindOne(ctx, bson.M{"username": user.Username}).Decode(&existingUser)
+	if err == nil {
+		http.Error(w, "User already exists", http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
+	user.Password = string(hashedPassword)
+
+	_, err = userCollection.InsertOne(ctx, user)
+	if err != nil {
+		http.Error(w, "Error creating user", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("User created"))
+}
